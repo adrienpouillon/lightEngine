fb6f44583ca56dce83f146fd821f4a41425ed3a6
adrienpouillon <apouillon@gaming.tech>
2025-02-14 07:49:44 +0100

    aaaaa

--- LightEngine/Plant.cpp
+++ LightEngine/Plant.cpp
@@ -39,170 +39,54 @@ void Plant::OnUpdate()
 void Plant::IaAction()
 {
 	Garden* garden = GetScene<Garden>();
-	if (garden->IsAlongLine(this) == false)
-	{
+	if (garden->IsAlongLine(this) == false){
 		//il y a un enemie
-		if (!garden->IsAreaEmpty(this, GetRadius() * 5))
-		{
+		if (!garden->IsAreaEmpty(this, GetRadius() * 5)){
 			//si enemie proche
-			if (GetCanShoot())
-			{
-				//on peut tirer
-				Shoot(TAGSHOOT2);
-			}
-			else if (GetCanReload())
-			{
-				//si peut super charger
-				SuperReload();
-			}
-		}
-		else
-		{
+			if (GetCanShoot()){/*on peut tirer*/Shoot(TAGSHOOT2);}
+			else if (GetCanReload()){/*si peut super charger*/SuperReload();}
+		}else{
 			//si pas d'enemie proche
-			if (GetCanShoot())
-			{
-				//on peut tirer
-				Shoot(TAGSHOOT1);
-			}
-			else if (GetCanReload())
-			{
-				//si peut recharger
-				Reload();
-			}
+			if (GetCanShoot()){/*on peut tirer*/Shoot(TAGSHOOT1);}
+			else if (GetCanReload()){/*si peut recharger*/Reload();}
 		}
 	}
 	else
-	{
-		//il y a personne sur la ligne
+	{//il y a personne sur la ligne
 		bool enemieUp = !garden->IsAlongLineUp(this);
 		bool enemieDown = !garden->IsAlongLineDown(this);
-		if (enemieUp && enemieDown)
-		{
-			//il y a un enemie sur la ligne du dessus et du dessous 
-			//if (garden->IsEmptyPlantInLineUp(this)) { //il y a un enemie sur la ligne du dessus et du dessous et pas de plante } 
-			if (!garden->IsAreaEmptyUp(this, GetRadius() * 5))
-			{
-				//il y a un enemie sur la ligne du dessus proche
-				if (!garden->IsAreaEmptyDown(this, GetRadius() * 5))
-				{
-					//il y a un enemie sur la ligne du dessous proche
-					//donc tuer un max enemie sur la ligne du dessus ou dessous
-					if (GetCanShoot())
-					{
-						//on peut tirer
-						int ran = GetScene()->GenerateRandomNumber(0, 1);
-						if (ran == 0)
-						{
-							//tirer dessus
-							Shoot(TAGSHOOT4);
-						}
-						else
-						{
-							//tirer dessous
-							Shoot(TAGSHOOT6);
-						}
-						
-					}
-					else if (GetCanReload())
-					{
-						//on peut recharger
-						Reload();
-					}
+		if (enemieUp && enemieDown){/*il y a un enemie sur la ligne du dessus et du dessous*/
+	//if (garden->IsEmptyPlantInLineUp(this)) { /*il y a un enemie sur la ligne du dessus et du dessous et pas de plante*/ } 
+			if (!garden->IsAreaEmptyUp(this, GetRadius() * 5)){/*il y a un enemie sur la ligne du dessus proche*/
+				if (!garden->IsAreaEmptyDown(this, GetRadius() * 5)){/*il y a un enemie sur la ligne du dessous proche donc tuer un max enemie sur la ligne du dessus ou dessous*/
+					if (GetCanShoot()){/*on peut tirer*/
+						if (GetScene()->GenerateRandomNumber(0, 1) == 0){/*tirer dessus*/Shoot(TAGSHOOT4);}
+						else{/*tirer dessous*/Shoot(TAGSHOOT6);}
+					}else if (GetCanReload()){/*on peut recharger*/Reload();}
+				}else{/*il y a un enemie sur la ligne du dessous mais pas proche donc tirer sur la ligne du dessus*/
+					if (GetCanShoot()){/*on peut tirer*/Shoot(TAGSHOOT3);}
+					else if (GetCanReload()){/*on peut recharger*/Reload();}
 				}
-				else
-				{
-					//il y a un enemie sur la ligne du dessous mais pas proche
-					//donc tirer sur la ligne du dessus
-					if (GetCanShoot())
-					{
-						//on peut tirer
-						Shoot(TAGSHOOT3);
-					}
-					else if (GetCanReload())
-					{
-						//on peut recharger
-						Reload();
-					}
-				}
-			}
-			else
-			{
-				//il y a un enemie sur la ligne du dessus mais pas proche
-				if (!garden->IsAreaEmptyDown(this, GetRadius() * 5))
-				{
-					//il y a un enemie sur la ligne du dessous proche
-					//donc tirer sur la ligne du dessous
-					if (GetCanShoot())
-					{
-						//on peut tirer
-						Shoot(TAGSHOOT5);
-					}
-					else if (GetCanReload())
-					{
-						//on peut recharger
-						Reload();
-					}
-				}
-				else
-				{
-					//il y a un enemie sur la ligne du dessous mais pas proche
-					//donc tirer sur la ligne du dessus ou dessous
-					if (GetCanShoot())
-					{
-						//on peut tirer
-						int ran = GetScene()->GenerateRandomNumber(0, 1);
-						if (ran == 0)
-						{
-							//tirer dessus
-							Shoot(TAGSHOOT3);
-						}
-						else
-						{
-							//tirer dessous
-							Shoot(TAGSHOOT5);
-						}
-					}
-					else if (GetCanReload())
-					{
-						//on peut recharger
-						Reload();
-					}
+			}else{/*il y a un enemie sur la ligne du dessus mais pas proche*/
+				if (!garden->IsAreaEmptyDown(this, GetRadius() * 5)){/*il y a un enemie sur la ligne du dessous proche donc tirer sur la ligne du dessous*/
+					if (GetCanShoot()){/*on peut tirer*/Shoot(TAGSHOOT5);}
+					else if (GetCanReload()){/*on peut recharger*/Reload();}
+				}else{/*il y a un enemie sur la ligne du dessous mais pas proche donc tirer sur la ligne du dessus ou dessous*/
+					if (GetCanShoot()){/*on peut tirer*/
+						if (GetScene()->GenerateRandomNumber(0, 1) == 0){/*tirer dessus*/Shoot(TAGSHOOT3);}
+						else{/*tirer dessous*/Shoot(TAGSHOOT5);}
+					}else if (GetCanReload()){/*on peut recharger*/Reload();}
 				}
 			}
 		}
 		else if (enemieUp)
-		{
-			//il y a un enemie sur la ligne du dessus
-			if (GetCanShoot())
-			{
-				//on peut tirer
-				Shoot(TAGSHOOT3);
-			}
-			else if (GetCanReload())
-			{
-				//on peut recharger
-				Reload();
-			}
-		}
-		else if (enemieDown)
-		{
-			//il y a un enemie sur la ligne du dessous
-			if (GetCanShoot())
-			{
-				//on peut tirer
-				Shoot(TAGSHOOT5);
-			}
-			else if (GetCanReload())
-			{
-				//on peut recharger
-				Reload();
-			}
-		}
-		else if (GetCanReload())
-		{
-			//on peut super charger
-			SuperReload();
-		}
+		{/*il y a un enemie sur la ligne du dessus*/
+			if (GetCanShoot()){/*on peut tirer*/Shoot(TAGSHOOT3);}
+			else if (GetCanReload()){/*on peut recharger*/Reload();}
+		}else if (enemieDown){/*il y a un enemie sur la ligne du dessous*/
+			if (GetCanShoot()){/*on peut tirer*/Shoot(TAGSHOOT5);}
+			else if (GetCanReload()){/*on peut recharger*/Reload();}
+		}else if (GetCanReload()){/*on peut super charger*/SuperReload();}
 	}
 }
 
